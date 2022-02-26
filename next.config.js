9e295bbe7f8b4ea74d8c80d2ff9c579fa4d69bf0
nix <john.nicholai.vigilia@gmail.com>
2022-02-26 14:42:53 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reactStrictMode: true,
+  images: { domains: ["res.cloudinary.com"] },
+  env: {
+    mongodburl: "Your MongoDB connection String",
+  },
+};
---
+++ pages/_document.js
@@ -0,0 +1,45 @@
+// fixing the UI styles when refreshing the page using MUI
+// To override the default Document, create the file pages/_document.js
+
+import { ServerStyleSheets } from "@material-ui/core/styles";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import React from "react";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+// This will be run before rendering the above components
+MyDocument.getInitialProps = async (ctx) => {
+  console.log("_document.js ctx", ctx);
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () => {
+    return originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+  };
+  const initialProps = await Document.getInitialProps(ctx);
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
